aaaf89f801d4d9eb8b3baa73c8c3d9e002aa780a
VishSeran <vishwa.sovis@gmail.com>
2026-07-28 21:55:17 +0530

    mcp tool updated

--- mcp_server/server_tools.py
+++ mcp_server/server_tools.py
@@ -18,8 +18,60 @@ async def addition(a:float, b:float) -> float:
         raise
                 
     except Exception as e:
-        print(f"Error in llm agent init: {e}")
+        print(f"Error in addition: {e}")
         raise
     
+@mcp.tool
+async def substract(a:float, b:float):
     
-async def substract(a:float, b:float)
+    try:
+            if a is None or b is None:
+                raise  ValueError("paramter a and b cannot be empty")
+            
+            return a - b
+            
+    except ValueError as e:
+        print(f"Value error: {e}")
+        raise
+                
+    except Exception as e:
+        print(f"Error in substraction: {e}")
+        raise
+    
+
+@mcp.tool
+async def multiply(a:float, b:float):
+    
+    try:
+            if a is None or b is None:
+                raise  ValueError("paramter a and b cannot be empty")
+            
+            return a * b
+            
+    except ValueError as e:
+        print(f"Value error: {e}")
+        raise
+                
+    except Exception as e:
+        print(f"Error in multiplication: {e}")
+        raise
+    
+@mcp.tool
+async def division(a:float, b:float):
+    
+    try:
+            if a is None or b is None:
+                raise  ValueError("paramter a and b cannot be empty")
+            
+            if b == 0:
+                raise ValueError("paramter b cannot be zero")
+            
+            return a/b
+            
+    except ValueError as e:
+        print(f"Value error: {e}")
+        raise
+                
+    except Exception as e:
+        print(f"Error in llm agent init: {e}")
+        raise
