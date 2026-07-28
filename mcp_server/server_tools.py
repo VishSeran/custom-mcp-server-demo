17b003e8b6c9d2f93d74fbfa239600c4d2e09a53
VishSeran <vishwa.sovis@gmail.com>
2026-07-28 21:52:43 +0530

    mcp tool updated

--- mcp_server/server_tools.py
+++ mcp_server/server_tools.py
@@ -1,11 +1,17 @@
-mcp = MCPServer()
+from mcp_server.server import MCPServer
 
+
+mcp_server = MCPServer()
+mcp = mcp_server.mcp_server
+
+@mcp.tool
 async def addition(a:float, b:float) -> float:
     
     try:
-        if not a or b:
-            raise ValueError("paramter a and b cannot be empty")
+        if a is None or b is None:
+            raise  ValueError("paramter a and b cannot be empty")
         
+        return a + b
         
     except ValueError as e:
         print(f"Value error: {e}")
@@ -14,3 +20,6 @@ async def addition(a:float, b:float) -> float:
     except Exception as e:
         print(f"Error in llm agent init: {e}")
         raise
+    
+    
+async def substract(a:float, b:float)
