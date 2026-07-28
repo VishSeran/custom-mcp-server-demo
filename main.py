53dcb78b0752d7c1c740712faaf0c858ec5691fe
VishSeran <vishwa.sovis@gmail.com>
2026-07-28 22:19:54 +0530

    main updated

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+from agents.llm_agent import LLMAgent
 from mcp_server.server import mcp
+import asyncio
+from fastmcp.client.transports import StdioTransport
+from fastmcp.client import Client
+
+
+async def main():
+    
+    try:
+        
+        
+        stdio_transport = StdioTransport(
+            command="python",
+            args=["mcp_server/mcp_stdio.py"]
+        )
+        
+        stdio_clinet = Client(stdio_transport)
+        
+        
+    except ValueError as e:
+            print(f"Value error: {e}")
+            raise
+            
+    except Exception as e:
+        print(f"Error in main: {e}")
+        raise    
 
 
 if __name__ == "__main__":
-    mcp.run()
+    asyncio.run(main())
